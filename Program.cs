81d60c20fadb1ca656a7dbf8d9181c4697984b88
Mario Gutierrez <mario@ls-hq.lasosl.com>
2026-02-20 12:39:10 +0100

    Agregar using para FormOptions

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http.Features;
 using FormacionesApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
